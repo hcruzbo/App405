41abbe295b8f757e412d241a316ed95b0d1cb6a8
Hector Cruz <hcruzbo@linfield.edu>
2016-05-25 14:57:12 -0700

    final

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,16 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
 resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 resources :microposts,          only: [:create, :destroy]
  #   get 'help'    => 'static_pages#help'
  
 #  get 'signup'  => 'users#new'
-
+  resources :relationships,       only: [:create, :destroy]
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
